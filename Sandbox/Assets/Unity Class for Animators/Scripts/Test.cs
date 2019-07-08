989298b5a24c6c4ef9b9069bb766b021d288384e
Khatim <kahtim.dar@gmail.com>
2019-07-08 10:24:49 +0400

    Intern task

---
+++ Sandbox/Assets/Unity Class for Animators/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+    private Rigidbody rg;
+
+    private Vector3 moveInput;
+    private Vector3 moveVel;
+
+    public Camera cam;
+
+
+    void Awake()
+    {
+        rg = GetComponent<Rigidbody>();
+        cam = cam.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        moveVel = moveInput * speed * Time.deltaTime;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(ray, out rayLength))
+        {
+            Vector3 pointToLook = ray.GetPoint(rayLength);
+            Debug.DrawLine(ray.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+}
